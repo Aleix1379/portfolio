e33cea08c0b5d122682b81137894855fd792ece3
Aleix <aleixmp1379@gmail.com>
2025-04-01 23:24:04 +0200

    Update tech stack references and fix JSX formatting
    Replaced "React JS" with "Vue.js" and added "TypeScript" to the list of technologies. Also adjusted JSX formatting for consistency and readability in the ProjectsSection component.

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -29,27 +29,30 @@ const ProjectsSection = () => {
             I like to work on personal projects to improve and learn new
             technologies, usually with
           </span>
-          <span className={styles.important}> React JS</span>,
+          <span className={styles.important}> Vue.js</span>,
           <span className={styles.important}> React Native </span>and
-          <span className={styles.important}> NodeJS</span>
+          <span className={styles.important}> NodeJS</span>,
+          <span> - all powered by </span>
+          <span className={styles.important}> TypeScript</span>
+          <span> ✨</span>
         </div>
       </div>
-      <div className={styles.projects}>
-        {projects.map(project => (
-          <Project
-            key={project.id}
-            name={project.name}
-            description={project.description}
-            platform={project.platform}
-            image={project.image}
-            links={project.links}
-            technologies={project.technologies}
-          />
-        ))}
-      </div>
-      {!showAllProjects && <Button onClick={fetchAllProjects}>Show All</Button>}
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Project
+              key={project.id}
+              name={project.name}
+              description={project.description}
+              platform={project.platform}
+              image={project.image}
+              links={project.links}
+              technologies={project.technologies}
+            />
+          ))}
+        </div>
+        {!showAllProjects && <Button onClick={fetchAllProjects}>Show All</Button>}
     </section>
-  )
+)
 }
 
 export default ProjectsSection
